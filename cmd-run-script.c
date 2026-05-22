575e9f424e8f52b63d403ce281f6890550361af2
Rasmus Thystrup Karstensen <rathka@gmail.com>
2026-05-23 01:07:02 +0200

    add run-script command
    New command run-script (alias: run-s) runs a script with arguments.
    Unlike run-shell which takes a single shell command string, run-script
    takes a script path followed by zero or more arguments, shell-quoting
    and joining them into a command string before execution.
    
    This allows command-alias to pass arguments to a script cleanly:
    
      set -g command-alias[0] s='run-script ~/.config/tmux/new-session.sh'
    
    Then :s myname passes myname as $1 to the script.

---
+++ cmd-run-script.c
@@ -0,0 +1,269 @@
+/* $OpenBSD$ */
+
+/*
+ * Copyright (c) 2009 Tiago Cunha <me@tiagocunha.org>
+ * Copyright (c) 2009 Nicholas Marriott <nicm@openbsd.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF MIND, USE, DATA OR PROFITS, WHETHER
+ * IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "tmux.h"
+
+/*
+ * Runs a script without a window, passing arguments to it.
+ */
+
+static enum cmd_retval		cmd_run_script_exec(struct cmd *,
+				    struct cmdq_item *);
+
+static void	cmd_run_script_timer(int, short, void *);
+static void	cmd_run_script_callback(struct job *);
+static void	cmd_run_script_free(void *);
+static void	cmd_run_script_print(struct job *, const char *);
+
+const struct cmd_entry cmd_run_script_entry = {
+	.name = "run-script",
+	.alias = "run-s",
+
+	.args = { "bd:t:Ec:", 1, INT_MAX, NULL },
+	.usage = "[-bE] [-c start-directory] [-d delay] " CMD_TARGET_PANE_USAGE
+	         " script [argument ...]",
+
+	.target = { 't', CMD_FIND_PANE, CMD_FIND_CANFAIL },
+
+	.flags = 0,
+	.exec = cmd_run_script_exec
+};
+
+struct cmd_run_script_data {
+	struct client			*client;
+	char				*cmd;
+	char				*cwd;
+	struct cmdq_item		*item;
+	struct session			*s;
+	int				 wp_id;
+	struct event			 timer;
+	int				 flags;
+};
+
+static void
+cmd_run_script_print(struct job *job, const char *msg)
+{
+	struct cmd_run_script_data	*cdata = job_get_data(job);
+	struct window_pane		*wp = NULL;
+	struct cmd_find_state		 fs;
+	struct window_mode_entry	*wme;
+
+	if (cdata->wp_id != -1)
+		wp = window_pane_find_by_id(cdata->wp_id);
+	if (wp == NULL) {
+		if (cdata->item != NULL) {
+			cmdq_print(cdata->item, "%s", msg);
+			return;
+		}
+		if (cdata->item != NULL && cdata->client != NULL)
+			wp = server_client_get_pane(cdata->client);
+		if (wp == NULL && cmd_find_from_nothing(&fs, 0) == 0)
+			wp = fs.wp;
+		if (wp == NULL)
+			return;
+	}
+
+	wme = TAILQ_FIRST(&wp->modes);
+	if (wme == NULL || wme->mode != &window_view_mode)
+		window_pane_set_mode(wp, NULL, &window_view_mode, NULL, NULL);
+	window_copy_add(wp, 1, "%s", msg);
+}
+
+static enum cmd_retval
+cmd_run_script_exec(struct cmd *self, struct cmdq_item *item)
+{
+	struct args			*args = cmd_get_args(self);
+	struct cmd_find_state		*target = cmdq_get_target(item);
+	struct cmd_run_script_data	*cdata;
+	struct client			*c = cmdq_get_client(item);
+	struct client			*tc = cmdq_get_target_client(item);
+	struct session			*s = target->s;
+	struct window_pane		*wp = target->wp;
+	const char			*delay;
+	double				 d;
+	struct timeval			 tv;
+	char				*end, *cmd, *escaped, *tmp;
+	u_int				 i;
+	int				 wait = !args_has(args, 'b');
+
+	if ((delay = args_get(args, 'd')) != NULL) {
+		d = strtod(delay, &end);
+		if (*end != '\0') {
+			cmdq_error(item, "invalid delay time: %s", delay);
+			return (CMD_RETURN_ERROR);
+		}
+	}
+
+	/* Build shell command string: escape and join all arguments. */
+	cmd = xstrdup("");
+	for (i = 0; i < args_count(args); i++) {
+		escaped = args_escape(args_string(args, i));
+		if (i == 0)
+			tmp = xstrdup(escaped);
+		else
+			xasprintf(&tmp, "%s %s", cmd, escaped);
+		free(escaped);
+		free(cmd);
+		cmd = tmp;
+	}
+
+	cdata = xcalloc(1, sizeof *cdata);
+	cdata->cmd = format_single_from_target(item, cmd);
+	free(cmd);
+
+	if (args_has(args, 't') && wp != NULL)
+		cdata->wp_id = wp->id;
+	else
+		cdata->wp_id = -1;
+
+	if (wait) {
+		cdata->client = c;
+		cdata->item = item;
+	} else {
+		cdata->client = tc;
+		cdata->flags |= JOB_NOWAIT;
+	}
+	if (cdata->client != NULL)
+		cdata->client->references++;
+	if (args_has(args, 'c'))
+		cdata->cwd = xstrdup(args_get(args, 'c'));
+	else
+		cdata->cwd = xstrdup(server_client_get_cwd(c, s));
+
+	if (args_has(args, 'E'))
+		cdata->flags |= JOB_SHOWSTDERR;
+
+	cdata->s = s;
+	if (s != NULL)
+		session_add_ref(s, __func__);
+
+	evtimer_set(&cdata->timer, cmd_run_script_timer, cdata);
+	if (delay != NULL) {
+		timerclear(&tv);
+		tv.tv_sec = (time_t)d;
+		tv.tv_usec = (d - (double)tv.tv_sec) * 1000000U;
+		evtimer_add(&cdata->timer, &tv);
+	} else
+		event_active(&cdata->timer, EV_TIMEOUT, 1);
+
+	if (!wait)
+		return (CMD_RETURN_NORMAL);
+	return (CMD_RETURN_WAIT);
+}
+
+static void
+cmd_run_script_timer(__unused int fd, __unused short events, void* arg)
+{
+	struct cmd_run_script_data	*cdata = arg;
+	struct client			*c = cdata->client;
+	const char			*cmd = cdata->cmd;
+
+	if (cmd == NULL) {
+		if (cdata->item != NULL)
+			cmdq_continue(cdata->item);
+		cmd_run_script_free(cdata);
+		return;
+	}
+	if (job_run(cmd, 0, NULL, NULL, cdata->s, cdata->cwd, NULL,
+	    cmd_run_script_callback, cmd_run_script_free, cdata,
+	    cdata->flags, -1, -1) == NULL) {
+		if (cdata->item == NULL)
+			status_message_set(c, -1, 1, 0, 0,
+			    "failed to run command: %s", cmd);
+		else {
+			cmdq_error(cdata->item,
+			    "failed to run command: %s", cmd);
+			cmdq_continue(cdata->item);
+		}
+		cmd_run_script_free(cdata);
+	}
+}
+
+static void
+cmd_run_script_callback(struct job *job)
+{
+	struct cmd_run_script_data	*cdata = job_get_data(job);
+	struct bufferevent		*event = job_get_event(job);
+	struct cmdq_item		*item = cdata->item;
+	char				*cmd = cdata->cmd, *msg = NULL, *line;
+	size_t				 size;
+	int				 retcode, status;
+
+	do {
+		line = evbuffer_readln(event->input, NULL, EVBUFFER_EOL_LF);
+		if (line != NULL) {
+			cmd_run_script_print(job, line);
+			free(line);
+		}
+	} while (line != NULL);
+
+	size = EVBUFFER_LENGTH(event->input);
+	if (size != 0) {
+		line = xmalloc(size + 1);
+		memcpy(line, EVBUFFER_DATA(event->input), size);
+		line[size] = '\0';
+
+		cmd_run_script_print(job, line);
+
+		free(line);
+	}
+
+	status = job_get_status(job);
+	if (WIFEXITED(status)) {
+		if ((retcode = WEXITSTATUS(status)) != 0)
+			xasprintf(&msg, "'%s' returned %d", cmd, retcode);
+	} else if (WIFSIGNALED(status)) {
+		retcode = WTERMSIG(status);
+		xasprintf(&msg, "'%s' terminated by signal %d", cmd, retcode);
+		retcode += 128;
+	} else
+		retcode = 0;
+	if (msg != NULL)
+		cmd_run_script_print(job, msg);
+	free(msg);
+
+	if (item != NULL) {
+		if (cmdq_get_client(item) != NULL &&
+		    cmdq_get_client(item)->session == NULL)
+			cmdq_get_client(item)->retval = retcode;
+		cmdq_continue(item);
+	}
+}
+
+static void
+cmd_run_script_free(void *data)
+{
+	struct cmd_run_script_data	*cdata = data;
+
+	evtimer_del(&cdata->timer);
+	if (cdata->s != NULL)
+		session_remove_ref(cdata->s, __func__);
+	if (cdata->client != NULL)
+		server_client_unref(cdata->client);
+	free(cdata->cwd);
+	free(cdata->cmd);
+	free(cdata);
+}
--- cmd.c
+++ cmd.c
@@ -89,6 +89,7 @@ extern const struct cmd_entry cmd_respawn_pane_entry;
 extern const struct cmd_entry cmd_respawn_window_entry;
 extern const struct cmd_entry cmd_rotate_window_entry;
 extern const struct cmd_entry cmd_run_shell_entry;
+extern const struct cmd_entry cmd_run_script_entry;
 extern const struct cmd_entry cmd_save_buffer_entry;
 extern const struct cmd_entry cmd_select_layout_entry;
 extern const struct cmd_entry cmd_select_pane_entry;
@@ -182,6 +183,7 @@ const struct cmd_entry *cmd_table[] = {
 	&cmd_respawn_window_entry,
 	&cmd_rotate_window_entry,
 	&cmd_run_shell_entry,
+	&cmd_run_script_entry,
 	&cmd_save_buffer_entry,
 	&cmd_select_layout_entry,
 	&cmd_select_pane_entry,
